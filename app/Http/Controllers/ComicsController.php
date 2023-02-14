edca601514715006316a6d094217ca5bd1705144
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-02-14 15:18:12 +0100

    Create function Create() in controller

--- app/Http/Controllers/ComicsController.php
+++ app/Http/Controllers/ComicsController.php
@@ -31,4 +31,8 @@ class ComicsController extends Controller
         return view('comics.show', compact('comic'));
     }
 
+    public function create() {
+        return view('comics.create');
+    }
+
 }
