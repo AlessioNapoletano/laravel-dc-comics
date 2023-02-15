304ff9862c80d71f28f5544257e060ae4e7f0e61
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-02-15 17:21:20 +0100

    Insert attribute placeholder in the input tag

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -15,40 +15,40 @@
     
         <div class="mb-3">
             <label for="title" class="form-label fw-bold">Titolo: </label>
-            <input type="text" id="title" name="title" class="form-control">
+            <input type="text" id="title" name="title" class="form-control" placeholder="Inserire il Titolo">
         </div>
 
         <div class="mb-3">
             <label for="description" class="form-label fw-bold">Descrizione: </label>
-            <input type="text" id="description" name="description" class="form-control">
+            <input type="text" id="description" name="description" class="form-control" placeholder="Inserire la descrizione">
         </div>
 
         <div class="mb-3">
             <label for="thumb" class="form-label fw-bold">Thumb URL: </label>
-            <input type="text" id="thumb" name="thumb" class="form-control">
+            <input type="text" id="thumb" name="thumb" class="form-control" placeholder="Inserire il path dell'immagine">
         </div>
 
         <div class="mb-3">
             <label for="price" class="form-label fw-bold">Prezzo: </label>
-            <input type="number" id="price" name="price" class="form-control">
+            <input type="number" id="price" name="price" class="form-control" placeholder="Inserire il prezzo">
         </div>
 
         <div class="mb-3">
             <label for="series" class="form-label fw-bold">Serie: </label>
-            <input type="text" id="series" name="series" class="form-control">
+            <input type="text" id="series" name="series" class="form-control" placeholder="Inserire la serie">
         </div>
 
         <div class="mb-3">
             <label for="sale_date" class="form-label fw-bold">Data Di Uscita: </label>
-            <input type="text" id="sale_date" name="sale_date" class="form-control">
+            <input type="text" id="sale_date" name="sale_date" class="form-control" placeholder="inserire la data di uscita">
         </div>
 
         <div class="mb-3">
             <label for="type" class="form-label fw-bold">Tipo: </label>
-            <input type="text" id="type" name="type" class="form-control">
+            <input type="text" id="type" name="type" class="form-control" placeholder="inserire il tipo">
         </div>
         
-        <button class="sumbit btn-primary btn">Invia</button>
+        <button class="btn-primary btn">Invia</button>
     </form>
 </div>
 
