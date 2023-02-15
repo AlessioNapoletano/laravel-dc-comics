8caa5becd406407c77ee15516b9a8481b0b75e56
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-02-15 17:43:49 +0100

    Create home page for Guests

--- app/Http/Controllers/Guest/HomeController.php
+++ app/Http/Controllers/Guest/HomeController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Comic as Comic;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function home() {
-        return view('comics.Guest.home');
+        $comics = Comic::all();
+        return view('comics.Guest.home', compact('comics'));
     }
 }
--- resources/views/comics/Guest/home.blade.php
+++ resources/views/comics/Guest/home.blade.php
@@ -4,8 +4,37 @@
 
 @section('main-content')
 
-<h1>
-    Questa pagina non è ancora stata creata, Collegarsi alla pagina per admin al link sottostante
-</h1>
+<header class="bg-primary text-light mb-3 py-3 d-flex justify-content-between aling-items-center">
+    <h1 class="d-inle ">
+        header
+    </h1>
 
-<a href="{{ route('comics.index') }}">Pagina per Admin</a>
+    <div class="button">
+        <a class="d-inline ms-end btn btn-success" href="{{ route('comics.index') }}">Pagina per Admin</a>  
+    </div>
+    
+</header>
+
+<main>
+    <section class="home-content">
+        <div class="container">
+            <div class="row">
+                @foreach ($comics as $comic)
+                    <div class="col-4">
+                        <div class="card mb-4">
+                            <img src="{{ $comic->thumb }}" class="card-img-top img-fluid" alt="{{ $comic->title }}">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $comic->title }}</h5>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </section>
+    
+</main>
+
+
+
+@endsection
