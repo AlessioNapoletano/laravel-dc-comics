80d0df5476c1970391b46a41587d786b58aeab34
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-02-16 15:47:41 +0100

    fix form in two pages (edit and create)

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -20,43 +20,45 @@
             </ul>
         </div>
     @endif
-    
+
     <form action="{{ route('comics.store') }}" method="POST">
         @csrf
     
         <div class="mb-3">
             <label for="title" class="form-label fw-bold">Titolo: </label>
-            <input type="text" id="title" name="title" class="form-control" placeholder="Inserire il Titolo">
+            <input type="text" id="title" name="title" class="form-control" placeholder="Inserire il Titolo" value="{{ old('title') }}">
         </div>
 
         <div class="mb-3">
             <label for="description" class="form-label fw-bold">Descrizione: </label>
-            <input type="text" id="description" name="description" class="form-control" placeholder="Inserire la descrizione">
+            <textarea type="text" id="description" name="description" class="form-control"> 
+                {{ old('description')}}
+            </textarea>
         </div>
 
         <div class="mb-3">
             <label for="thumb" class="form-label fw-bold">Thumb URL: </label>
-            <input type="text" id="thumb" name="thumb" class="form-control" placeholder="Inserire il path dell'immagine">
+            <input type="text" id="thumb" name="thumb" class="form-control" placeholder="Inserire il path dell'immagine" value="{{ old('thumb') }}">
         </div>
 
         <div class="mb-3">
             <label for="price" class="form-label fw-bold">Prezzo: </label>
-            <input type="number" id="price" name="price" class="form-control" placeholder="Inserire il prezzo">
+            <input type="number" id="price" name="price" class="form-control" placeholder="Inserire il prezzo" value="{{ old('price') }}">
         </div>
 
         <div class="mb-3">
             <label for="series" class="form-label fw-bold">Serie: </label>
-            <input type="text" id="series" name="series" class="form-control" placeholder="Inserire la serie">
+            <input type="text" id="series" name="series" class="form-control" placeholder="Inserire la serie" value="{{ old('series') }}">
         </div>
 
         <div class="mb-3">
             <label for="sale_date" class="form-label fw-bold">Data Di Uscita: </label>
-            <input type="text" id="sale_date" name="sale_date" class="form-control" placeholder="inserire la data di uscita">
+            <input type="text" id="sale_date" name="sale_date" class="form-control" placeholder="inserire la data di uscita" value="{{ old('sale_date') }}">
         </div>
 
         <div class="mb-3">
             <label for="type" class="form-label fw-bold">Tipo: </label>
-            <input type="text" id="type" name="type" class="form-control" placeholder="inserire il tipo">
+            <input type="text" id="type" name="type" class="form-control" placeholder="inserire il tipo" value="{{ old('type') }}">
         </div>
         
         <button class="btn-primary btn">Invia</button>
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -19,37 +19,44 @@
 
         <div class="mb-3">
             <label for="title" class="form-label fw-bold">Titolo: </label>
-            <input type="text" id="title" name="title" class="form-control" value="{{ $comic->title }}">
+            <input type="text" id="title" name="title" class="form-control" 
+            value="{{ old('title') ?? $comic->title }}">
         </div>
 
         <div class="mb-3">
             <label for="description" class="form-label fw-bold">Descrizione: </label>
-            <input type="text" id="description" name="description" class="form-control" value="{{ $comic->description }}">
+            <input type="text" id="description" name="description" class="form-control" 
+            value="{{ old('description') ?? $comic->description }}">
         </div>
 
         <div class="mb-3">
             <label for="thumb" class="form-label fw-bold">Thumb URL: </label>
-            <input type="text" id="thumb" name="thumb" class="form-control" value="{{ $comic->thumb }}">
+            <input type="text" id="thumb" name="thumb" class="form-control" 
+            value="{{ old('thumb') ?? $comic->thumb }}">
         </div>
 
         <div class="mb-3">
             <label for="price" class="form-label fw-bold">Prezzo: </label>
-            <input type="number" id="price" name="price" class="form-control" value="{{ $comic->price }}">
+            <input type="number" id="price" name="price" class="form-control" 
+            value="{{ old('price') ?? $comic->price }}">
         </div>
 
         <div class="mb-3">
             <label for="series" class="form-label fw-bold">Serie: </label>
-            <input type="text" id="series" name="series" class="form-control" value="{{ $comic->series }}">
+            <input type="text" id="series" name="series" class="form-control" 
+            value="{{ old('series') ?? $comic->series }}">
         </div>
 
         <div class="mb-3">
             <label for="sale_date" class="form-label fw-bold">Data Di Uscita: </label>
-            <input type="text" id="sale_date" name="sale_date" class="form-control" value="{{ $comic->sale_date }}">
+            <input type="text" id="sale_date" name="sale_date" class="form-control" 
+            value="{{ old('sale_date') ?? $comic->sale_date }}">
         </div>
 
         <div class="mb-3">
             <label for="type" class="form-label fw-bold">Tipo: </label>
-            <input type="text" id="type" name="type" class="form-control" value="{{ $comic->type }}">
+            <input type="text" id="type" name="type" class="form-control" 
+            value="{{ old('type') ?? $comic->type }}">
         </div>
         
         <button type="sumbit" class="btn-primary btn">Invia</button>
